4dd61ff4ed211a6d69b6616ccb5b7d0dea765d81
imseano <soreta@csu.fullerton.edu>
2023-05-10 10:24:00 -0700

    Full sudoku grid added

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-print("hi there")
 from array import *
 # Sudoku board represented by a 2d array
 # 0 = empty space
@@ -18,11 +17,15 @@ for y in range(3):
     for x in range(3):
         s_row_val = (3 * y) + x # Current sudoku row
         row = "║"
-        for z in Problem[s_row_val]:
-            num = str(z)
-            if z == 0:
+        for z in range(9):
+            num = str(Problem[s_row_val][z])
+            if Problem[s_row_val][z] == 0:
                 num = " "
-            row += " " + num + " │"
+            row += " " + num + " "
+            if (z + 1) % 3 == 0:
+                row += "║"  
+            else:
+                row += "│"
             #print("║   │   │   ║   │   │   ║   │   │   ║")
         print(row)
         if y == 2 and x == 2:
@@ -31,3 +34,4 @@ for y in range(3):
             print("╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣")
         else:
             print("╟───┼───┼───╫───┼───┼───╫───┼───┼───╢")
+    
