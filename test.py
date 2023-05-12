c2d0ebcdc45b3878fd157fa8a4d1913701f6932f
imseano <soreta@csu.fullerton.edu>
2023-05-11 23:37:06 -0700

    Replaced sudoku example problem with an actual sudoku puzzle and created a test main menu.

--- test.py
+++ test.py
@@ -2,24 +2,38 @@ from array import *
 # Sudoku board represented by a 2d array
 # 0 = empty space
 Problem = [
-[8,5,0,0,2,4,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
-[7,2,0,0,0,0,0,0,0],
+[3,0,6,5,0,8,4,0,0],
+[5,2,0,0,0,0,0,0,0],
+[0,8,7,0,0,0,0,3,1],
+[0,0,3,0,1,0,0,8,0],
+[9,0,0,8,6,3,0,0,5],
+[0,5,0,0,9,0,6,0,0],
+[1,3,0,0,0,0,2,5,0],
+[0,0,0,0,0,0,0,7,4],
+[0,0,5,2,0,6,3,0,0],
 ]
 
 
 
 def main():
-    print("Hello World")
-    printSudoku()
+    print("SUDOKU")
+    print("Press 1 to start")
+    while True:
+        gamemode = input()
+        try:
+            gamemode = int(gamemode)
+           # gamemode = 1
+        except:
+            print("Invalid input")
+            continue
+        if gamemode != 1:
+            print("Invalid number")
+            continue
+        break
 
-def printSudoku():
+    printSudokuTable()
+
+def printSudokuTable():
     print("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗");
     for y in range(3):
         for x in range(3):
