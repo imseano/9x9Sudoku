2d19f4db4fac2e098c74ab3551bfc88c3366040f
imseano <soreta@csu.fullerton.edu>
2023-05-11 23:18:53 -0700

    Put the sudoku table print in its own function

--- test.py
+++ test.py
@@ -12,26 +12,35 @@ Problem = [
 [7,2,0,0,0,0,0,0,0],
 [7,2,0,0,0,0,0,0,0],
 ]
-print("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗");
-for y in range(3):
-    for x in range(3):
-        s_row_val = (3 * y) + x # Current sudoku row
-        row = "║"
-        for z in range(9):
-            num = str(Problem[s_row_val][z])
-            if Problem[s_row_val][z] == 0:
-                num = " "
-            row += " " + num + " "
-            if (z + 1) % 3 == 0:
-                row += "║"  
+
+
+
+def main():
+    print("Hello World")
+    printSudoku()
+
+def printSudoku():
+    print("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗");
+    for y in range(3):
+        for x in range(3):
+            s_row_val = (3 * y) + x # Current sudoku row
+            row = "║"
+            for z in range(9):
+                num = str(Problem[s_row_val][z])
+                if Problem[s_row_val][z] == 0:
+                    num = " "
+                row += " " + num + " "
+                if (z + 1) % 3 == 0:
+                    row += "║"  
+                else:
+                    row += "│"
+                #print("║   │   │   ║   │   │   ║   │   │   ║")
+            print(row)
+            if y == 2 and x == 2:
+                print("╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝")
+            elif x == 2:
+                print("╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣")
             else:
-                row += "│"
-            #print("║   │   │   ║   │   │   ║   │   │   ║")
-        print(row)
-        if y == 2 and x == 2:
-            print("╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝")
-        elif x == 2:
-            print("╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣")
-        else:
-            print("╟───┼───┼───╫───┼───┼───╫───┼───┼───╢")
+                print("╟───┼───┼───╫───┼───┼───╫───┼───┼───╢")
     
+main()
