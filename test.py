05f27d6a1fb3c2bfb4183e958aa48a511fbfc681
imseano <soreta@csu.fullerton.edu>
2023-05-12 00:48:54 -0700

    Rudementary Insert system for sudoku

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ Problem = [
 [0,0,5,2,0,6,3,0,0],
 ]
 
-selected = [0,0] # Selected space (x,y)
+selected = [0,1] # Selected space (x,y)
 
 
 def main():
@@ -33,7 +33,7 @@ def main():
     game()
     exitGame()
 
-def printSudokuTable():
+def printSudokuTable(Problem):
     print("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗");
     for y in range(3):
         for x in range(3):
@@ -42,7 +42,10 @@ def printSudokuTable():
             for z in range(9):
                 num = str(Problem[s_row_val][z])
                 if Problem[s_row_val][z] == 0:
-                    num = " "
+                    if s_row_val == selected[0] and z == selected[1]:
+                        num = "X"
+                    else: 
+                        num = " "
                 row += " " + num + " "
                 if (z + 1) % 3 == 0:
                     row += "║"  
@@ -58,14 +61,70 @@ def printSudokuTable():
                 print("╟───┼───┼───╫───┼───┼───╫───┼───┼───╢")
 
 def game():
+    Game_Board = Problem
     running = True
     while running:
-        printSudokuTable()
+        printSudokuTable(Game_Board)
+        print(selected)
         print("Press Q to quit")
+        print("Press u, d, l, r to move to cursor")
+        print("Input a # from 1 - 9 to add it to the square")
         choice = input("Your next move: ")
         match choice:
             case "Q" :
                 running = False
+            case "u" :
+                search_up = selected[0]
+                for y in range(9):
+                    search_up -= 1
+                    if search_up < 0:
+                        search_up += 9
+                    print(search_up)
+                    if Game_Board[search_up][selected[1]] == 0:
+                        selected[0] = search_up
+                        break
+            case "d" :
+                search_down = selected[0]
+                for y in range(9):
+                    search_down += 1
+                    if search_down > 8:
+                        search_down -= 9
+                    print(search_down)
+                    if Game_Board[search_down][selected[1]] == 0:
+                        selected[0] = search_down
+                        break
+            case "l" :
+                search_left = selected[1]
+                for x in range(9):
+                    search_left -= 1
+                    if search_left < 0:
+                        search_left += 9
+                    print(search_left)
+                    if Game_Board[selected[0]][search_left] == 0:
+                        selected[1] = search_left
+                        break
+            case "r" :
+                search_right = selected[1]
+                for x in range(9):
+                   search_right += 1
+                   if search_right > 8:
+                       search_right -= 9
+                   print(search_right)
+                   if Game_Board[selected[0]][search_right] == 0:
+                       selected[1] = search_right
+                       break
+            case "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" :
+                print("inserted a " + str(choice) + " at " + str(selected))
+                Game_Board[selected[0]][selected[1]] = int(choice)
+                search_right = selected[1]
+                for x in range(9):
+                   search_right += 1
+                   if search_right > 8:
+                       search_right -= 9
+                   print(search_right)
+                   if Game_Board[selected[0]][search_right] == 0:
+                       selected[1] = search_right
+                       break
             case _ :
                 print("Invalid")
 
