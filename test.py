0edad7a1b05225ca19b2a84d8cd9f8a73dd5c271
Tokkisan <74226172+Tokkisan@users.noreply.github.com>
2023-05-15 23:48:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -156,7 +156,61 @@ def findNearestEmptySpace(Board):
         search_row += 1
         if search_row > 8:
             search_row -= 9
+            
+            
+            
+def check(player_board):
+    if check_for0(player_board) == True:
+        a1 = [
+            [3,1,6,5,7,8,4,9,2],
+            [5,2,9,1,3,4,7,6,8],
+            [4,8,7,6,2,9,5,3,1],
+            [2,6,3,4,1,5,9,8,7],
+            [9,7,4,8,6,3,1,2,5],
+            [8,5,1,7,9,2,6,4,3],
+            [1,3,8,9,4,7,2,5,6],
+            [6,9,2,3,5,1,8,7,4],
+            [7,4,5,2,8,6,3,1,9],
+            ]
     
+        for i, row in enumerate(player_board):
+            #dup = duplicates
+            dup = ifdup(row)
+            if dup:
+                print(f"Duplicate values at row {i+1}  at position:{dup}")
+                
+        if player_board == a1:
+            print("correct answer")
+        elif player_board != a1:
+            print("wrong answer")
+    elif checkfor0(playerboard) == False:
+        print("not complete")
+
+#check if any duplicates within an array
+def ifdup(array):
+    #create dictionary exist
+    exist = {}
+    #duplicate list to store positions
+    dup = []
+    for i, num in enumerate(array):
+        #set !=0 since our placeholders are 0
+        if num !=0:
+            #if the number exists in the dictionary append to duplicate list with its positions
+            if num in exist:
+                dup.append(i)
+                dup.append(exist[num])
+            else:
+                exist[num] = i
+    return dup
+#check if there are any empty spaces        
+def check_for0(Board):
+    for row in Board:
+        if 0 in row:
+            print("not complete")
+            return False
+    print("complete")
+    return True
+              
 
 def exitGame():
     print("Game Over! Thanks for playing!")
